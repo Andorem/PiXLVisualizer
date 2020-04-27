c68d8ced0b1f5f8fe1715298ab54738062115020
Alexis Siguenza <alexis.sigue@gmail.com>
2020-04-27 15:34:24 -0700

    Updated color legend to use relative values - Should fix issue with some elements (e.g. Calcium) not showing correctly - Added more info to tooltip: coordinates, relative and absolute values

---
+++ public/js/utils/continuousLegend.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/** Modified version of d3 legend library @ https://gist.github.com/syntagmatic/e8ccca52559796be775553b467593a9f */
+function continuous(selector_id, colorscale) {
+  var legendheight = 200,
+      legendwidth = 80,
+      margin = {
+    top: 10,
+    right: 60,
+    bottom: 10,
+    left: 2
+  }; // Modified to allow selection of dynamic svg elements
+
+  var foreignObject = d3.select(selector_id).append("foreignObject").attr("x", margin.x).attr("y", margin.y).attr("width", legendwidth).attr("height", legendheight);
+  var foBody = foreignObject.append("xhtml:body").style("margin", "0px").style("padding", "0px").style("background-color", "transparent").style("width", legendwidth + "px").style("height", legendheight + "px");
+  var canvas = foBody.style("height", legendheight + "px").style("width", legendwidth + "px").style("position", "relative").append("canvas").attr("height", legendheight - margin.top - margin.bottom).attr("width", 1).style("height", legendheight - margin.top - margin.bottom + "px").style("width", legendwidth - margin.left - margin.right + "px").style("border", "1px solid #000").style("position", "absolute").style("top", margin.top + "px").style("left", margin.left + "px").node();
+  var ctx = canvas.getContext("2d");
+  var legendscale = d3.scaleLinear().range([legendheight - margin.top - margin.bottom, 1]).domain(colorscale.domain()); // image data hackery based on http://bl.ocks.org/mbostock/048d21cf747371b11884f75ad896e5a5
+
+  var image = ctx.createImageData(1, legendheight);
+  d3.range(legendheight).forEach(function (i) {
+    var c = d3.rgb(colorscale(legendscale.invert(i)));
+    image.data[4 * i] = c.r;
+    image.data[4 * i + 1] = c.g;
+    image.data[4 * i + 2] = c.b;
+    image.data[4 * i + 3] = 255;
+  });
+  ctx.putImageData(image, 0, 0); // A simpler way to do the above, but possibly slower. keep in mind the legend width is stretched because the width attr of the canvas is 1
+  // See http://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
+  // d3.range(legendheight).forEach(function(i) {
+  //   ctx.fillStyle = colorscale(legendscale.invert(i));
+  //   ctx.fillRect(0,i,1,1);
+  // });
+
+  var legendaxis = d3.axisRight().scale(legendscale).tickSize(6).ticks(8);
+  var svg = d3.select(selector_id).append("svg").attr("height", legendheight + "px").attr("width", legendwidth + "px").style("position", "absolute").style("left", "0px").style("top", "0px");
+  svg.append("g").attr("class", "axis").attr("transform", "translate(" + (legendwidth - margin.left - margin.right + 3) + "," + margin.top + ")").call(legendaxis);
+}
+
+;
--- src/assets/js/element.js
+++ src/assets/js/element.js
@@ -2,19 +2,46 @@
 
 let width = 752;
 let height = 580;
-let heatmap = d3.select('#heatmap').attr('style', `width: ${width}; height: ${height};`);
+
+let scaleMultiplier = 1.5;
+let heatmap = d3.select('#heatmap-svg');//.attr('transform', `scale(${scaleMultiplier})`);
 
 let data = JSON.parse(atob(heatmap.attr("data")));
 heatmap.attr('data', ""); // clear passed data
 
+ // Render original image
+ var heatmapImage = heatmap.append("svg:image")
+ .attr("xlink:href", "../img/main.jpg")
+ .attr('class', 'img-fluid');
 
+ // Render heatmap overlay
+ var overlay = heatmap.append("svg")
+ .attr('class', 'overlay')
+ .attr('x', 0)
+ .attr('y', 0)
+
+ // Zoom and pan
+heatmap.call(d3.zoom()
+.scaleExtent([1, 10])
+.translateExtent([[0, 0], [width, height]])
+.extent([[0, 0], [width, height]])
+.on("zoom", function () {
+    heatmap.attr("transform", d3.event.transform);
+ }));
+
+// Detector Selection Dropdown
+let spinner = $("#detector-dropdown-group .spinner-border");
 $(document).ready(function() {
     $("#detector-dropdown a").click(function(e) {
         e.preventDefault();
         var detectorName = $(this).attr('data');
         $('#detector-dropdown-group button').text($(this).text());
         $("#detector-dropdown-group button").attr('data', detectorName);
-        update(detectorName);
+        spinner.attr('display', 'block');
+        setTimeout(function() {
+            update(detectorName);
+            spinner.attr('display', 'none');
+        }, 100);
     });
 });
 
@@ -24,60 +51,135 @@ function update(name) {
     updateHeatmap(detectorValues);
 }
 
-// Render original image
-heatmap.append("svg:image")
-    .attr("xlink:href", "../img/main.jpg")
-    .attr('class', 'img-fluid');
 
-// Heatmap Color Range
-var getColor = d3.scaleSequential()
-.interpolator(d3.interpolateInferno)
-.domain([1,100]);
 
 // Get data for heatmap overlay
 function updateHeatmap(values) {
-    // Size scales
-    let overlayHeight = Math.abs(values[values.length - 1].coords.y - values[0].coords.y);
-    let overlayWidth = Math.abs(values[values.length - 1].coords.x - values[0].coords.x);
-    console.log(`overlay dimensions: ${overlayHeight} = h, ${overlayWidth} = w`);
+    
+    // Get coordinates as data arrays
+    let xCoords = values.map((value) => {return parseFloat(value.coords.x)}).sort();
+    let yCoords = values.map((value) => {return parseFloat(value.coords.y)}).sort();
+    let absValues = values.map((value) => {return parseFloat(value.absolute)}).sort();
+    let relValues = values.map((value) => {return parseFloat(value.relative)}).sort();
 
-    let xCoords = values.map((value) => {return value.coords.x}).sort();
-    let yCoords = values.map((value) => {return value.coords.y}).sort();
-    console.log(`overlay coords: x = ${xCoords}, y = ${yCoords}`);
+    let max = {
+        x: d3.max(xCoords),
+        y: d3.max(yCoords),
+        abs: d3.max(absValues),
+        rel: d3.max(relValues)
+    };
 
-    // X
-    var xScale = d3.scaleBand()
-    .domain(xCoords)
-    .range([overlayWidth, 0])
-    .padding(0.4);
-    // Y
-    var yScale = d3.scaleBand()
-    .domain(yCoords)
-    .range([0, overlayHeight])
-    .padding(0.1)
+    let min = {
+        x: d3.min(xCoords),
+        y: d3.min(yCoords),
+        abs: d3.min(absValues),
+        rel: d3.min(relValues)
+    };
+    // console.log(`overlay coords: x = ${xCoords}, y = ${yCoords}`);
 
-    d3.select("#heatmap").append("text")
-    .attr("x", overlayWidth/2)
-    .attr("y", 0)
+    // Overlay Dimensions
+    let overlayHeight = Math.abs(yCoords[yCoords.length - 1] - yCoords[0]);
+    let overlayWidth = Math.abs(xCoords[xCoords.length - 1] - xCoords[0]);    
+    let horizontalCenter = (xCoords[0] + xCoords[xCoords.length - 1]) / 2;
+    let verticalCenter = (yCoords[0] + yCoords[yCoords.length - 1]) / 2;
+    // console.log(`overlay dimensions: ${overlayHeight} = h, ${overlayWidth} = w`);
+
+    // Heatmap Title
+    var title = overlay.append("text")
+    .attr("x", horizontalCenter)
+    .attr("y", yCoords[0] - 10)
     .attr("text-anchor", "middle")
     .style("font-size", "20px")
     .style('fill', 'white')
-    .text("Heatmap");
+    .style('font-weight', 'bold')
+    .style('stroke', 'black')
+    .text(`${data.type.abbreviation} Heatmap`);
 
-    d3.select("#heatmap").selectAll('rect')
+    // Heatmap Tooltip 
+    var heatmapWrapper = d3.select("#heatmap-wrapper");
+    var tooltip = ($("#heatmap-tooltip").length ?  // don't create duplicate legend
+        d3.select("#heatmap-tooltip")
+        :
+        heatmapWrapper.append("div")	
+        .attr("id", "heatmap-tooltip")				
+        .style("display", "none"));
+    
+    // Heatmap Color Range
+    var getColor = d3.scaleSequential()
+    .interpolator(d3.interpolateMagma)
+    .domain([0, max.rel]);
+
+    // X Scale
+    var xScale = d3.scaleBand()
+    .domain(xCoords)
+    .range([0, overlayWidth])
+    .paddingInner(20).paddingOuter(10/2);
+    
+    // Y Scale
+    var yScale = d3.scaleBand()
+        .domain(yCoords)
+        .range([0, overlayHeight])
+        .paddingInner(.2).paddingOuter(.2);
+
+ 
+// Legend
+    var legendColorScale = d3.scaleSequential(d3.interpolateMagma)
+    .domain([0, max.rel]);
+
+    var legend = overlay.append("svg")
+        .attr("id", "heatmap-legend");
+    continuous("#heatmap-legend", legendColorScale);
+    var legend = d3.select("#heatmap-legend")
+        .attr('y', overlayHeight + "px")
+        .attr('x', (xCoords[0] - 75) + "px");
+
+    // Legend Title
+    var title = overlay.append("text")
+    .attr("x", (xCoords[0] - 50) + "px")
+    .attr("y", overlayHeight + "px")
+    .attr("text-anchor", "middle")
+    .style("font-size", "12px")
+    .style('fill', 'black')
+    .style('font-weight', 'bold')
+    .style('stroke', 'black')
+    .text(`${data.type.abbreviation}_%`);
+
+    // Data Points
+    overlay.selectAll('rect')
     .data(values)
     .enter()
     .append("rect")
-      .attr("x", function(d) { return d.coords.x })
-      .attr("y", function(d) { return d.coords.y })
-      .attr("width", xScale.bandwidth() )
-      .attr("height", yScale.bandwidth() )
-      .style("fill", function(d) { return getColor(d.absolute)} )
+    .style("class", "heatmap-data")
+      .attr("x", function(d) { return parseFloat(d.coords.x) ; })
+      .attr("y", function(d) { return parseFloat(d.coords.y); })
+      .attr("width", 3)
+      .attr("height",3 )
+      .style("fill", function(d) { return getColor(d.relative)} )
       .style("stroke-width", 4)
       .style("stroke", "none")
-      .style("opacity", 0.8)
-      .append("text")
-      .attr("text-anchor", "middle") 
-      .attr("transform", "translate("+ (10/2) +","+(5/2)+")rotate(-90)") 
-      .text(function(d) {return d.absolute});
+      .style("opacity", 0.8) 
+      .on("mouseover", function(d) {
+            tooltip.html(`<b>X:</b> ${d.coords.x}, <b>Y:</b> ${d.coords.y}<br>
+                <b>Relative:</b> ${d.relative}<br>
+                <b>Absolute:</b> ${d.absolute}
+            `);
+            tooltip.style("display", null)
+                .style("left", (d3.mouse(heatmapWrapper.node())[0] + 20) + "px") // mouse relative to heatmap
+                .style("top", (d3.mouse(heatmapWrapper.node())[1]) + "px")
+                .style("background-color", getColor(d.relative));
+            if ((d.relative / max.rel) > .80) tooltip.style("color", "black"); // make text easier to see for light color values
+            else tooltip.style("color", "white");
+        })
+      .on("mouseout", function(d) { tooltip.style("display", "none"); });
+   
+}
+
+function multiplier(values, multiplier) {
+    let newValues = (old) => {
+        Object.keys(old).forEach((key) => {
+            old[key].coords.x = old[key].coords.x * multiplier;  
+            old[key].coords.y = old[key].coords.y * multiplier; 
+        });
+        return newValues;
+      }
 }
