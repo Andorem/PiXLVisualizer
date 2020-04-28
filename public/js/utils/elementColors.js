fc889ab09b0b0d9b767b1eb847e8fe254e2e647d
Alexis Siguenza <alexis.sigue@gmail.com>
2020-04-27 20:33:37 -0700

    Added distinct colors for each element

---
+++ public/js/utils/elementColors.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var ELEMENT_COLORS = {
+  "Si": d3.interpolateOranges,
+  "Al": d3.interpolatePurples,
+  "Fe": d3.interpolateReds,
+  "Ca": d3.interpolateBlues,
+  "Ti": d3.interpolateGreens,
+  "Mg": d3.interpolatePuRd
+};
+
+function getElementColor(abbr) {
+  return ELEMENT_COLORS[abbr];
+}
